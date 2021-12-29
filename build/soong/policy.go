83b174ea9fba407d2069304a43abbfa3a89bbf04
Yaroslav Zviezda <acroreiser@gmail.com>
2021-12-29 11:41:03 +0300

    sepolicy: allow su domain in user builds
    Do not use for anything except boot time one shot scripts.
    
    Change-Id: I3ac61e9e3e9741d512ece95c86a8e7ee1617a926

--- build/soong/policy.go
+++ build/soong/policy.go
@@ -558,7 +558,7 @@ func (c *policyBinary) GenerateAndroidBuildActions(ctx android.ModuleContext) {
 		rule.Temporary(permissiveDomains)
 
 		msg := `==========\n` +
-			`ERROR: permissive domains not allowed in user builds\n` +
+			`WARNING: permissive domains not allowed in user builds\n` +
 			`List of invalid domains:`
 
 		rule.Command().Text("if test").
@@ -568,7 +568,7 @@ func (c *policyBinary) GenerateAndroidBuildActions(ctx android.ModuleContext) {
 			Text(`"` + msg + `"`).
 			Text("&& cat ").
 			Input(permissiveDomains).
-			Text("; exit 1; fi")
+			Text("; fi")
 	}
 
 	out := pathForModuleOut(ctx, c.stem())
